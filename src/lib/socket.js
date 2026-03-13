8b47deec867db6c97b5e09196bcbc10d8bb86fc0
Begonet Debebe <begonetdebebe@gmail.com>
2026-03-13 23:50:58 +0300

    deployment

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,7 +1,8 @@
 import { io } from "socket.io-client";
 
-const socket = io(import.meta.env.VITE_API_URL, {
-  withCredentials: true,
-});
+const SOCKET_URL =
+  import.meta.env.VITE_API_URL || "http://localhost:5000";
+
+const socket = io(SOCKET_URL);
 
 export default socket;
