5eb1dd361db5cb60bd40e2b061c58ddb3a2a3d44
Begonet Debebe <begonetdebebe@gmail.com>
2026-03-14 19:10:45 +0300

    satur day update

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_API_URL;
+if (!API_URL) {
+  console.error("VITE_API_URL is not set. API requests will fail.");
+}
+
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  baseURL: `${API_URL}/api`,
 });
 
 api.interceptors.request.use(
